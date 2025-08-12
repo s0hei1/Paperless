8b3210cb1c23d69dfd5f8d5e9dea53f02409dcd8
Soheil <hesarisoheil1999@example.com>
2025-08-12 11:46:31 +0330

    make sqlalchemy to modern approach

--- apps/paperless/data/models/models.py
+++ apps/paperless/data/models/models.py
@@ -5,24 +5,25 @@ from sqlalchemy import (
     Enum,
     ForeignKey,
     DateTime,
-    Float,
     Boolean,
 )
-from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
 from datetime import datetime, UTC
 from apps.paperless.data.enums.approval_status import ApprovalStatus
 from apps.paperless.data.enums.unit_of_measure import UOMs
 from apps.paperless.data.enums.user_rolls import UserRoll
 
-SQLAlchemyModel = declarative_base()
+
+class SQLAlchemyModel(DeclarativeBase):
+    pass
 
 
 class Department(SQLAlchemyModel):
     __tablename__ = "departments"
-    id = Column(Integer, primary_key=True)
-    name = Column(String(128), nullable=False, unique=True)
-    code = Column(Integer, nullable=False, unique=True)
-    manager_id = Column(ForeignKey("users.id", use_alter=True), nullable=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(128), unique=True)
+    code: Mapped[int] = mapped_column(unique=True)
+    manager_id: Mapped[int | None] = mapped_column(ForeignKey("users.id", use_alter=True))
 
     users = relationship(
         "User", back_populates="department", foreign_keys="User.department_id"
@@ -35,13 +36,13 @@ class Department(SQLAlchemyModel):
 class User(SQLAlchemyModel):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
-    user_name = Column(String(128), nullable=False, unique=True)
-    password = Column(String(128), nullable=False)
-    user_roll = Column(Enum(UserRoll), nullable=False, default=UserRoll.PERSONNEL.value)
+    first_name: Mapped[str] = mapped_column(String(32))
+    last_name: Mapped[str] = mapped_column(String(32))
+    user_name: Mapped[str] = mapped_column(String(32), unique=True)
+    password: Mapped[str] = mapped_column(String(32))
+    user_roll : Mapped[UserRoll] = mapped_column(default=UserRoll.PERSONNEL)
 
-    department_id = Column(ForeignKey("departments.id"), nullable=False)
+    department_id : Mapped[int]= mapped_column(ForeignKey("departments.id"), nullable=False)
 
     department = relationship(
         "Department",
@@ -58,32 +59,31 @@ class User(SQLAlchemyModel):
 
 class GoodsExitApproval(SQLAlchemyModel):
     __tablename__ = "approvals"
-    id = Column(Integer, primary_key=True)
-    status = Column(Enum(ApprovalStatus), nullable=False)
-    user_id = Column(ForeignKey("users.id"), nullable=False)
-    doc_id = Column(ForeignKey("goods_exit_docs.id"), nullable=False)
-    modification_date_time = Column(DateTime, default=datetime.now(UTC))
+    id : Mapped[int] = mapped_column(primary_key=True)
+    status : Mapped[ApprovalStatus]
+    user_id : Mapped[int]= mapped_column(ForeignKey("users.id"))
+    doc_id : Mapped[int]= mapped_column(ForeignKey("goods_exit_docs.id"))
+    modification_date_time : Mapped[datetime]= mapped_column(default=datetime.now(UTC))
     user = relationship("User", back_populates="goods_exit_approvals")
     doc = relationship("GoodsExitDoc", back_populates="approvals")
 
 
 class GoodsExitDoc(SQLAlchemyModel):
     __tablename__ = "goods_exit_docs"
-    id = Column(Integer, primary_key=True)
-    doc_code = Column(String(8), nullable=False, unique=True)
-    creation_date_time = Column(DateTime, default=datetime.now(UTC))
-    sending_department_id = Column(ForeignKey("departments.id"))
-    sending_department_name = Column(String(128), nullable=False)
-    exit_reason = Column(String(256), nullable=False)
-    destination = Column(String(128), nullable=False)
-    address_ = Column(String(512), nullable=False)
-    sending_user_fullname = Column(String(128), nullable=False)
-    exit_for_ever = Column(Boolean, nullable=False)
-    receiver_name = Column(String(128), nullable=True)
-    status = Column(Enum(ApprovalStatus), nullable=False)
-
-    approval_status = Column(Enum(ApprovalStatus), nullable=False)
-    creator_user_id = Column(ForeignKey("users.id"))
+    id : Mapped[int ]= mapped_column(primary_key=True)
+    doc_code : Mapped[str]= mapped_column(String(8), unique=True)
+    creation_date_time : Mapped[datetime]= mapped_column(default=datetime.now(UTC))
+    sending_department_id : Mapped[int] = mapped_column(ForeignKey("departments.id"))
+    sending_department_name : Mapped[str] = mapped_column(String(128))
+    exit_reason : Mapped[str] = mapped_column(String(256))
+    destination : Mapped[str] = mapped_column(String(128))
+    address_ : Mapped[str] = mapped_column(String(512))
+    sending_user_fullname : Mapped[str] = mapped_column(String(128))
+    exit_for_ever : Mapped[bool]
+    receiver_name : Mapped[str | None] = mapped_column(String(128))
+    status : Mapped[ApprovalStatus]
+    approval_status : Mapped[ApprovalStatus]
+    creator_user_id : Mapped[int] = mapped_column(ForeignKey("users.id"))
 
     approvals = relationship("GoodsExitApproval", back_populates="doc")
     creator_user = relationship("User", backref="goods_exit_docs")
@@ -93,11 +93,11 @@ class GoodsExitDoc(SQLAlchemyModel):
 
 class GoodsExit(SQLAlchemyModel):
     __tablename__ = "goods_exits"
-    id = Column(Integer, primary_key=True)
-    description = Column(String(256), nullable=False)
-    sap_code = Column(String(16), nullable=True)
-    count = Column(Integer, nullable=False)
-    unit_of_measure = Column(Enum(UOMs), nullable=False)
-    goods_exit_doc_id = Column(ForeignKey("goods_exit_docs.id"), nullable=False)
+    id : Mapped[int] = mapped_column(Integer, primary_key=True)
+    description : Mapped[str]= mapped_column(String(256))
+    sap_code : Mapped[str | None]= mapped_column(String(16))
+    count : Mapped[int]
+    unit_of_measure : Mapped[UOMs]
+    goods_exit_doc_id : Mapped[int] = mapped_column(ForeignKey("goods_exit_docs.id"))
 
     goods_exit_doc = relationship("GoodsExitDoc", back_populates="items")
